451f994b619e31947592c54ff52d18a3b072f525
Mayank Singh Tomar <143412700+mayank-singh-tomar@users.noreply.github.com>
2024-03-09 17:53:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -216,6 +216,6 @@ def logout_google():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5432))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+   # app.run(debug=True)
